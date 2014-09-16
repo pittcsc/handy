843b90bbdd9ebb745987a3bb9266faa5b0ef48b2
Joel Roggeman <joelroggeman@gmail.com>
2014-09-16 18:14:55 -0400

    Modified route naming

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'members/text_dispatch'
+  # get 'members/text_dispatch'
+  get '/text', to: 'members#text_dispatch'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
