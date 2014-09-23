138f1cd04b8ae667ef9f42d0b4884cc087023ee0
Joel Roggeman <joelroggeman@gmail.com>
2014-09-23 16:02:07 -0400

    Updated event.rb and removed the current_event controller

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,15 @@
 class Event < ActiveRecord::Base
+  before_save :ensure_one_current
+
+  def self.current
+    find_by_current true
+  end
+
+  private
+    def ensure_one_current
+      if current_changed? && current?
+        self.class.current.try(:update!, current: false)
+      end
+    end
+
 end
