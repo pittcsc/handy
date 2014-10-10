6b4b35184227f6641932d80ed06be1780e9a9768
Joel Roggeman <joelroggeman@gmail.com>
2014-10-10 17:04:14 -0400

    Heavily refactored SMS processing

---
+++ app/controllers/sms/inbound_messages_controller.rb
@@ -0,0 +1,19 @@
+class InboundMessagesController < ApplicationController
+
+  # Disable XSRF protection to allow the Twilio API to make POST requests on the server
+  skip_before_action :verify_authenticity_token
+
+  def create
+    InboundMessageProcessor.new(phone_number, body).process
+  end
+
+  private
+    def phone_number
+      params[:From]
+    end
+
+    def body
+      params[:Body]
+    end
+end
+
---
+++ app/models/inbound_message_processor.rb
@@ -0,0 +1,68 @@
+require 'twilio-ruby'
+
+class InboundMessageProcessor
+
+  def intialize(phone_number, body)
+    @phone_number = phone_number
+    @body = body
+  end
+
+  def process
+    if member
+      if member.email
+        create_attendance_for_current_event
+      else
+        update_member_email
+      end
+    else
+      create_member
+    end
+  end
+
+  private
+    def client
+      @client ||= Twilio::REST::Client.new Twilio_Keys[:account_sid], Twilio_Keys[:auth_token]
+    end
+
+    def member
+      @member ||= Member.find_by(phone: @phone_number)
+    end
+
+    def event
+      @event ||= Event.where(token: event_token).current
+    end
+
+    def event_token
+      @body.strip.downcase
+    end
+
+    def create_attendance_for_event
+      if event
+        unless Attendance.exists?(event: event, member: member)
+          Attendance.create(member: member, event: event)
+        end
+
+        respond "Welcome to #{event.name}!"
+      else
+        respond "Invalid event code."
+      end
+    end
+
+    def update_member_email
+      Member.update!(email: @body)
+      respond "Thanks, #{member.first_name}! Your email was updated to #{@body}."
+    end
+
+    def create_member
+      Member.create!(phone: @phone_number, name: @body)
+      respond "Hey there, #{member.first_name}!  Welcome to Handy.  Please respond with your email address."
+    end
+
+    def respond(response)
+      client.messages.create(
+        from: Twilio_Keys[:phone_number],
+        to: phone_number,
+        body: @body
+      )
+    end
+end
