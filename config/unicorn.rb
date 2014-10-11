065df559a9bfd79f11c7df5c73d7a92491e4255f
George Claghorn <gc@georgeclaghorn.com>
2014-10-10 20:48:22 -0400

    Configure Unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,16 @@
+preload_app true
+worker_count 2
+timeout 30
+
+check_client_connection true
+
+listen '/u/apps/handy/shared/sockets/unicorn.sock', backlog: 2048
+pid '/u/apps/handy/shared/pids/unicorn.pid'
+stderr_path '/u/apps/bcx/shared/log/unicorn.log'
+stdout_path '/u/apps/bcx/shared/log/unicorn.log'
+
+working_directory '/u/apps/handy/current'
+
+after_fork do |server, worker|
+  ActiveRecord::Base.clear_all_connections! if defined?(ActiveRecord::Base)
+end
