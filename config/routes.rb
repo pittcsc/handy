9108df5cfa44cd6757eee524e84c1fa8622ce28b
Joel Roggeman <joelroggeman@gmail.com>
2014-10-10 21:13:18 -0400

    Updated routing

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
     resources :attendances
   end
 
+  root to: 'events#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
