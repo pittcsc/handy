ca79fb8104a5f58308cb454ec2199beb2f8f1f80
George Claghorn <gc@georgeclaghorn.com>
2014-10-11 15:34:17 -0400

    Update deploy config
    Keep only the latest five releases on the server. Delete unused staging config.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
-# config valid only for Capistrano 3.1
+# This config is valid only for Capistrano 3.1.
 lock '3.2.1'
 
 set :application, 'Handy'
 set :deploy_to, '/u/apps/handy'
+set :keep_releases, 5
 
 # Git
 set :scm, :git
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-role :app, %w{george@app-01}
-role :web, %w{george@app-01}
-role :db,  %w{george@app-01}
+role :app, %w{app-01}
+role :web, %w{app-01}
+role :db,  %w{app-01}
 
 set :ssh_options,
   proxy: Net::SSH::Proxy::Command.new('ssh gateway.do-nyc3.pittcsc.org -q -W %h:%p'),
