3168a255bc8d98e07ca775e4102684eec14c6f27
Joel Roggeman <joelroggeman@gmail.com>
2014-10-10 22:09:56 -0400

    Altered Twilio configuration code

--- app/jobs/sms/delivery_job.rb
+++ app/jobs/sms/delivery_job.rb
@@ -9,6 +9,14 @@ class Sms::DeliveryJob < ActiveJob::Base
  
   private
     def client
-      @client ||= Twilio::REST::Client.new
+      @client ||= Twilio::REST::Client.new account_sid, auth_token
+    end
+
+    def account_sid
+      Rails.configuration.x.twilio[:account_sid]
+    end
+
+    def auth_token
+      Rails.configuration.x.twilio[:auth_token]
     end
 end
