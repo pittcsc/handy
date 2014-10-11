37ff85485eb6b5b87cb765180fa1071d2d23734a
George Claghorn <gc@georgeclaghorn.com>
2014-10-11 15:33:54 -0400

    Give Unicorn processes obvious titles

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,5 +12,11 @@ stdout_path '/u/apps/handy/shared/log/unicorn.log'
 working_directory '/u/apps/handy/current'
 
 after_fork do |server, worker|
+  # Give processes a nice title.
+  revision = File.read('/u/apps/handy/current/REVISION').slice(0, 6)
+  ENV['UNICORN_PROCTITLE'] = "handy-#{revision}"
+  $0 = ENV['UNICORN_PROCTITLE']
+
+  # Close old MySQL connections.
   ActiveRecord::Base.clear_all_connections! if defined?(ActiveRecord::Base)
 end
