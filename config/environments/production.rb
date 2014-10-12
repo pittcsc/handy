f4e78111bd7c4c663518363a4da4af5a440e80b9
George Claghorn <gc@georgeclaghorn.com>
2014-10-12 11:32:55 -0400

    Use debug log level in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
@@ -79,6 +79,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
- 
+
   config.active_job.queue_adapter = :resque
 end
