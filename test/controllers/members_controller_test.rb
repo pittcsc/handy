ce766ef081ac41e693efb56ec5bd794b10bafdc7
George Claghorn <gc@georgeclaghorn.com>
2014-10-16 23:02:14 -0400

    Delete unused tests

--- test/controllers/members_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class MembersControllerTest < ActionController::TestCase
-  test "should get dispatch" do
-    get :dispatch
-    assert_response :success
-  end
-
-end
--- test/helpers/current_events_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CurrentEventsHelperTest < ActionView::TestCase
-end
