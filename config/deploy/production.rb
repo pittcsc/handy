1e469865f6277e86297bc81a0fe291c8702f0daa
George Claghorn <gc@georgeclaghorn.com>
2014-10-26 20:20:51 -0400

    Deploy with app-specific user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,4 @@
-role :app, %w{app-01}
-role :web, %w{app-01}
-role :db,  %w{app-01}
+server 'app-01', roles: %w{app web db}, user: 'handy'
 
 set :ssh_options,
   proxy: Net::SSH::Proxy::Command.new('ssh gateway.do-nyc3.pittcsc.org -q -W %h:%p'),
