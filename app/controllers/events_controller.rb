d90d6197a8e763d1322bf79ccadfa9e8f8f6c8da
George Claghorn <gc@georgeclaghorn.com>
2014-11-17 01:05:06 -0500

    Order events newest to oldest

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :set_event, except: [:index, :new, :create]
 
   def index
-    @events = Event.all
+    @events = Event.order(date: :desc)
     @current = Event.current
   end
 
