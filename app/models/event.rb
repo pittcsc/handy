dfba6f2cda94db85e2e695a217d920309c0c05eb
Joel Roggeman <joelroggeman@gmail.com>
2014-11-16 19:00:50 -0500

    General clean up

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,32 +1,27 @@
 class Event < ActiveRecord::Base
-  before_validation :generate_token, on: :create
-  has_many :attendances, dependent: :destroy
-  has_many :registrations, dependent: :destroy
+	has_many :attendances, dependent: :destroy
+	has_many :registrations, dependent: :destroy
 
-  before_validation :generate_token, on: :create
-  validates :token, presence: true, uniqueness: true
+	before_validation :generate_token, on: :create
+	validates :token, presence: true, uniqueness: true
 
-  validates :name, presence: true
-  validates :date, presence: true
+	validates :name, presence: true
+	validates :date, presence: true
 
-  before_save :ensure_one_current
+	before_save :ensure_one_current
 
-  def self.current
-    find_by_current true
-  end
-
-  private
-    def ensure_one_current
-      if current_changed? && current?
-        self.class.current.try(:update!, current: false)
-      end
-    end
-
-    def generate_token
-      self.token = Token.generate(2)
-    end
-
-    def list
-    	@list ||= File.readlines('nouns.txt').each(&:strip!)
+	def self.current
+		find_by_current true
 	end
+
+	private
+		def ensure_one_current
+			if current_changed? && current?
+				self.class.current.try(:update!, current: false)
+			end
+		end
+
+		def generate_token
+			self.token = Token.generate(2)
+		end
 end
--- app/models/event/token.rb
+++ app/models/event/token.rb
@@ -1,15 +1,14 @@
 class Event::Token
+	DICTIONARY_FILE_PATH = Rails.root.join('share', 'nouns.txt')
 
-  DICTIONARY_FILE_PATH = Rails.root.join('share', 'nouns.txt')
+	class << self
+		def generate(words)
+			dictionary.sample(words).join(' ')
+		end
 
-  class << self
-  	def generate(words)
-      dictionary.sample(words).join(' ')
-    end
-
-    private
-      def dictionary
-      	@dictionary ||= File.readlines(DICTIONARY_FILE_PATH).each(&:strip!)
-      end
-  end
+		private
+			def dictionary
+				@dictionary ||= File.readlines(DICTIONARY_FILE_PATH).each(&:strip!)
+			end
+	end
 end
