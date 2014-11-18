f52207f732afa4beefbb79d63b13e3e69f19c8dc
Joel Roggeman <joelroggeman@gmail.com>
2014-11-17 23:30:57 -0500

    Require both email and password in model, unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
 
   def self.authenticate(email, password)
     find_by_email(email).try(:authenticate, password)
