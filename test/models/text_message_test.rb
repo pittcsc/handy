17eda3dfe6a9edf08e4a1ae27f0d8fd14e2ca14a
George Claghorn <gc@georgeclaghorn.com>
2014-11-25 18:11:27 -0500

    Add tests for TextMessage

---
+++ test/models/text_message_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class TextMessageTest < ActiveSupport::TestCase
+  test 'finds member' do
+    assert_equal members(:michael), text_messages(:michael_checking_in).member
+  end
+
+  test 'finds no member for new registrants' do
+    assert_equal nil, text_messages(:rashid_checking_in).member
+  end
+
+  test 'finds registration' do
+    assert_equal registrations(:rashid), text_messages(:rashid_checking_in).registration
+  end
+
+  test 'processes as check-in with member' do
+    TextMessage::AttendanceProcessor.any_instance.expects(:process).once
+
+    text_messages(:michael_checking_in).process
+  end
+
+  test 'processes as registration without member' do
+    TextMessage::RegistrationProcessor.any_instance.expects(:process).once
+
+    text_messages(:rashid_checking_in).process
+  end
+
+  test 'responds' do
+    Sms::DeliveryJob.expects(:perform_later).with(text_messages(:michael_checking_in).phone_number, 'Message received!')
+
+    text_messages(:michael_checking_in).respond('Message received!')
+  end
+end
