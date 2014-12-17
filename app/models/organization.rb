702a2d9ab8521dac3a3fe3a68ee61afdb1b74ac1
Joel Roggeman <joelroggeman@gmail.com>
2014-12-17 14:08:15 -0500

    Organizations require a name

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -3,4 +3,6 @@ class Organization < ActiveRecord::Base
   has_many :users, through: :accesses
   has_many :event_ownerships, dependent: :destroy
   has_many :events, through: :event_ownerships
+
+  validates :name, presence: true
 end
