8c7365fdb0e091613bbcd8db2b7c235e13e84371
George Claghorn <george.claghorn@gmail.com>
2015-01-28 14:28:20 -0500

    Use special port for gateway SSH

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server 'app-01', roles: %w{app web db}, user: 'handy'
 
 set :ssh_options,
-  proxy: Net::SSH::Proxy::Command.new('ssh gateway.do-nyc3.pittcsc.org -q -W %h:%p'),
+  proxy: Net::SSH::Proxy::Command.new('ssh gateway.do-nyc3.pittcsc.org -q -W %h:%p -p 56362'),
   forward_agent: true
