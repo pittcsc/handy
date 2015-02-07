7d7346a4f6b25c1da170e6cdffacce8334c079ac
George Claghorn <george.claghorn@gmail.com>
2015-02-07 16:25:11 -0500

    Fix local wkhtmltopdf path

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -3,5 +3,5 @@ require 'wicked_pdf'
 WickedPdf.config = if Rails.env.production?
   { exe_path: '/u/apps/handy/shared/bin/wkhtmltopdf' }
 else
-  { exe_path: 'wkhtmltopdf' }
+  { exe_path: '/usr/local/bin/wkhtmltopdf' }
 end
