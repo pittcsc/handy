6c58441c47c091d6a64c4c719b09ca127a798e30
George Claghorn <george.claghorn@gmail.com>
2015-03-30 08:20:17 -0400

    Don't allocate a new Twilio::REST::Client object for every text message that we send

--- app/models/text_message/sender.rb
+++ app/models/text_message/sender.rb
@@ -1,10 +1,15 @@
 class TextMessage::Sender
+  def self.client
+    @@client ||= Twilio::REST::Client.new
+  end
+  delegate :client, to: :class
+
   def initialize(text_message)
     @text_message = text_message
   end
 
   def send
-    Twilio::REST::Client.new.messages.create \
+    client.messages.create \
       from: Rails.configuration.x.twilio[:phone_number],
       to: @text_message.phone_number,
       body: @text_message.body
