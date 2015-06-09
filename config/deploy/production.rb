a46b945a120100085a241e936f7d581f729d8ca5
George Claghorn <george.claghorn@gmail.com>
2015-06-08 22:49:26 -0400

    Temporarily connect to proxy verbosely

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 server 'app-01', roles: %w(app web db), user: 'handy'
 
 set :ssh_options,
-  proxy: Net::SSH::Proxy::Command.new('ssh handy@gateway.do-nyc3.pittcsc.org -q -W %h:%p -p 56362'),
+  proxy: Net::SSH::Proxy::Command.new('ssh handy@gateway.do-nyc3.pittcsc.org -q -W %h:%p -p 56362 -vvvv'),
   forward_agent: true
