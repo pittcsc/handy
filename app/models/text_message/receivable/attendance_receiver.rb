a91cfb6ad50ecf852c27ca04b5afd513f9842e00
George Claghorn <george@basecamp.com>
2015-10-25 04:39:30 -0400

    Rename

--- app/models/text_message/receivable/attendance_receiver.rb
+++ app/models/text_message/receivable/attendance_receiver.rb
@@ -6,13 +6,13 @@ module TextMessage::Receivable
 
     def receive
       with_active_event_by_token do |event|
-        create_attendance event
+        mark_sender_as_present_at event
         respond "Thanks, #{text_message.member.first_name}. You're checked in for #{event.name}."
       end
     end
 
     private
-      def create_attendance(event)
+      def mark_sender_as_present_at(event)
         event.attendances.find_or_create_by!(member: text_message.member)
       end
   end
