cff28644069a023ab1115eacb1c1eb4ad4a272d8
George Claghorn <george@basecamp.com>
2015-10-25 04:37:56 -0400

    Hoist
    Let create_attendance do what it says it's going to do, and nothing
    more. No side effects.

--- app/models/text_message/receivable/attendance_receiver.rb
+++ app/models/text_message/receivable/attendance_receiver.rb
@@ -7,13 +7,13 @@ module TextMessage::Receivable
     def receive
       with_active_event_by_token do |event|
         create_attendance event
+        respond "Thanks, #{text_message.member.first_name}. You're checked in for #{event.name}."
       end
     end
 
     private
       def create_attendance(event)
         event.attendances.find_or_create_by!(member: text_message.member)
-        respond "Thanks, #{text_message.member.first_name}. You're checked in for #{event.name}."
       end
   end
 end
