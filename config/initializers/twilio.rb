3c2ebb2fa598348ee2c03ac6b1e8b43a2bdabd18
George Claghorn <george@basecamp.com>
2015-10-25 12:08:17 -0400

    Keep all Twilio config in the Twilio initializer

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,5 +1,7 @@
 require 'twilio-ruby'
 
+Rails.configuration.x.twilio = Rails.application.config_for(:twilio).symbolize_keys
+
 Twilio.configure do |config|
   config.account_sid = Rails.configuration.x.twilio[:account_sid]
   config.auth_token = Rails.configuration.x.twilio[:auth_token]
